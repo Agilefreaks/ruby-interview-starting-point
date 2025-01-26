a3df44880c05d44ad17bfd9dcc39950a80b1674e
Salanoid <salanoid@gmail.com>
2025-01-26 16:14:00 +0200

    fix: nest PermitParams in a RodaPlugins module for Zeitwerk coc

--- app/roda_plugins/permit_params.rb
+++ app/roda_plugins/permit_params.rb
@@ -2,12 +2,14 @@
 
 require 'roda'
 
-module PermitParams
-  module InstanceMethods
-    def permit_params(params, allowed_keys)
-      params.slice(*allowed_keys)
+module RodaPlugins
+  module PermitParams
+    module InstanceMethods
+      def permit_params(params, allowed_keys)
+        params.slice(*allowed_keys)
+      end
     end
   end
 end
 
-Roda::RodaPlugins.register_plugin(:permit_params, PermitParams)
+Roda::RodaPlugins.register_plugin(:permit_params, RodaPlugins::PermitParams)
