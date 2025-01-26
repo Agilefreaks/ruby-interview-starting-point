5a6cfa335314e7937e37d6a1bd0911230e720b5e
Salanoid <salanoid@gmail.com>
2025-01-26 19:39:45 +0200

    feat: implement actual fetching and parsing of coffee shop data from CSV

--- app/api/v1/coffee_shops_route.rb
+++ app/api/v1/coffee_shops_route.rb
@@ -5,8 +5,19 @@ module Api
     class CoffeeShopsRoute < BaseRoute
       route do |r|
         r.get do
-          permit_params = permit_params(r.params, %w[x y])
-          puts permit_params
+          csv_url = ENV.fetch('COFFEE_SHOPS_CSV_URL', 'https://raw.githubusercontent.com/Agilefreaks/test_oop/master/coffee_shops.csv')
+          user_coordinates = permit_params(r.params, %w[x y]).transform_keys(&:to_sym)
+          csv_data = Services::FetchCsvService.new(csv_url).call
+          coffee_shops = Services::ParseCsvService.new(csv_data).call
+          coffee_shop_service = Services::CoffeeShopService.new(user_coordinates, coffee_shops)
+          closest_shops = coffee_shop_service.call
+
+          closest_shops.map do |shop|
+            {
+              name: shop[:name],
+              distance: shop[:distance]
+            }
+          end
         end
       end
     end
